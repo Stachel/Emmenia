fc68dc5f9d90e157938ee196589803d14a00010e
RinaStachel <hempa@inbox.ru>
2015-05-08 00:23:01 +0300

    small fix

--- app/src/main/java/ru/rinastachel/emmenia/fragments/AddFragment.java
+++ app/src/main/java/ru/rinastachel/emmenia/fragments/AddFragment.java
@@ -1,13 +1,6 @@
 package ru.rinastachel.emmenia.fragments;
 
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import ru.rinastachel.emmenia.R;
-
+/*
 public class AddFragment extends Fragment{
 
     public static AddFragment getInstance() {
@@ -21,3 +14,4 @@ public class AddFragment extends Fragment{
         return view;
     }
 }
+*/
